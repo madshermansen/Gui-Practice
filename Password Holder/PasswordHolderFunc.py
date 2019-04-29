5a2299aa6d08378b90397a1e8698a0e600f8f47a
Mads Hermansen <33220981+KarlofKuwait@users.noreply.github.com>
2019-04-29 17:35:09 +0200

    Update
    - FindPassword(), FindUsernames(), and Findshortcut() added.

--- Password Holder/PasswordHolderFunc.py
+++ Password Holder/PasswordHolderFunc.py
@@ -1,32 +1,44 @@
 #Add function and corresponding code
 
-def add(): #Add Shortcut, Password, and Username to directory
+def add(): #Add Shortcut, Password, and Usernames to directory
     Shortcut = input("Shortcut: ")
-    Username = input("Username: ")
+    Usernames = input("Username: ")
     Password = input("Password: ")
     Shortcutwrite(Shortcut)
-    Usernamewrite(Username)
+    Usernameswrite(Usernames)
     Passwordwrite(Password)
 
 def Shortcutwrite(Shortcut): #Write Shortcut in Shorcut.txt
-    typeShort = open("Shortcuts.txt", "w")
-    Keepdata = [x.strip() for x in open("Shortcuts.txt").readlines()]
+    try:
+        Keepdata = [x.strip() for x in open("Shortcuts.txt").readlines()]
+        typeShort = open("Shortcuts.txt", "w")
+    except:
+        typeShort = open("Shortcuts.txt", "w")
+        Keepdata = [x.strip() for x in open("Shortcuts.txt").readlines()]
     for Info in Keepdata:
         typeShort.write(str(Info) + "\n")
     typeShort.write(Shortcut + "\n")
     typeShort.close()
 
-def Usernamewrite(Username): #Write Username in Username.txt
-    typeUser = open("Username.txt", "w")
-    Keepdata = [x.strip() for x in open("Username.txt").readlines()]
+def Usernameswrite(Usernames): #Write Usernames in Usernames.txt
+    try:
+        Keepdata = [x.strip() for x in open("Usernames.txt").readlines()]
+        typeUser = open("Usernames.txt", "w")
+    except:
+        typeUser = open("Usernames.txt", "w")
+        Keepdata = [x.strip() for x in open("Usernames.txt").readlines()]
     for Info in Keepdata:
         typeUser.write(str(Info) + "\n")
-    typeUser.write(Username + "\n")
+    typeUser.write(Usernames + "\n")
     typeUser.close()
 
 def Passwordwrite(Password): #Write Password in Passwords.txt
-    typePass = open("Passwords.txt", "w")
-    Keepdata = [x.strip() for x in open("Passwords.txt").readlines()]
+    try:
+        Keepdata = [x.strip() for x in open("Passwords.txt").readlines()]
+        typePass = open("Passwords.txt", "w")
+    except:
+        typePass = open("Passwords.txt", "w")
+        Keepdata = [x.strip() for x in open("Passwords.txt").readlines()]
     for Info in Keepdata:
         typePass.write(str(Info) + "\n")
     typePass.write(Password + "\n")
@@ -40,26 +52,50 @@ def remove(): #Remove data from directory
 #Find function and correspondding code
 
 def find():
-    Shortcut = input("What is the shortcut?: ")
-    if not(Shortcut == ""):
-        return
-    Username = input("What is the username?: ")
-    if not(Username == ""):
-        return
-    Password = input("What is the password?: ")
-    if not(Password == ""):
-        return
+    global Keepdata
+    global Keepdata2
+    global Keepdata3
+    Keepdata = [x.strip() for x in open("Shortcuts.txt").readlines()]
+    Keepdata2 = [x.strip() for x in open("Usernames.txt").readlines()]
+    Keepdata3 = [x.strip() for x in open("Passwords.txt").readlines()]
+    while True:
+        Shortcut = input("What is the shortcut?: ")
+        if not(Shortcut == ""):
+            print("")
+            Findshortcut(Shortcut)
+            break
+        Usernames = input("What is the username?: ")
+        if not(Usernames == ""):
+            print("")
+            FindUsernames(Usernames)
+            break
+        Password = input("What is the password?: ")
+        if not(Password == ""):
+            print("")
+            Findpassword(Password)
+            break
+        print("")
 
 def Findshortcut(Shortcut):
-    return
+    print("Shortcut: Username:Password \n")
+    for Info in range(len(Keepdata)):
+        if Shortcut == Keepdata[Info]:
+            print(Keepdata[Info] + ": " + Keepdata2[Info] + ":" + Keepdata3[Info])
+           
 
-def Findusername(Username):
-    return
+def FindUsernames(Usernames):
+    print("Shortcut: Username:Password \n")
+    for Info in range(len(Keepdata)):
+        if Usernames == Keepdata2[Info]:
+            print(Keepdata[Info] + ": " + Keepdata2[Info] + ":" + Keepdata3[Info])
 
 def Findpassword(Password):
-    return
+    print("Shortcut: Username:Password \n")
+    for Info in range(len(Keepdata)):
+        if Password == Keepdata3[Info]:
+            print(Keepdata[Info] + ": " + Keepdata2[Info] + ":" + Keepdata3[Info])
 
 #Open function and corresponding code
 
-def open():
+def Open():
     input("")
