fd813c820844fc2b4beecf7c25b65e20d76bc9f1
Mads Hermansen <33220981+KarlofKuwait@users.noreply.github.com>
2019-04-29 17:06:14 +0200

    Update

--- Password Holder/Password Holder.py
+++ Password Holder/Password Holder.py
@@ -4,3 +4,5 @@ Function = input("What function do you want to use?: ")
 
 if Function == "Add" or Function == "add":
     PasswordHolderFunc.add()
+elif Function == "Find" or Function == "find":
+    PasswordHolderFunc.find()
