357b94195990aee9be446f7c13898facd2be547d
Mads Hermansen <33220981+KarlofKuwait@users.noreply.github.com>
2019-04-30 20:43:04 +0200

    Update
    Shortened
    Remove Function

--- Password Holder/PasswordHolderFunc.py
+++ Password Holder/PasswordHolderFunc.py
@@ -1,4 +1,5 @@
 #Add function and corresponding code
+import PySimpleGUI as sg
 
 def add(): #Add Shortcut, Password, and Usernames to directory
     Shortcut = input("Shortcut: ")
@@ -46,8 +47,55 @@ def Passwordwrite(Password): #Write Password in Passwords.txt
 
 #Remove function and corresponding code
 
-def remove(): #Remove data from directory
-    input("")
+def remover(): #Remove data from directory
+    global Keepdata
+    global Keepdata2
+    global Keepdata3
+    global RemoveIndex
+    RemoveIndex = []
+    Keepdata = [x.strip() for x in open("Shortcuts.txt").readlines()]
+    Keepdata2 = [x.strip() for x in open("Usernames.txt").readlines()]
+    Keepdata3 = [x.strip() for x in open("Passwords.txt").readlines()]
+    while True:
+        Shortcut = input("What is the shortcut?: ")
+        if not(Shortcut == ""):
+            print("")
+            Remover(Shortcut, Keepdata)
+            break
+        Usernames = input("What is the username?: ")
+        if not(Usernames == ""):
+            print("")
+            Remover(Usernames, Keepdata2)
+            break
+        Password = input("What is the password?: ")
+        if not(Password == ""):
+            print("")
+            Remover(Password, Keepdata3)
+            break
+        print("")
+
+def Remover(Value, Data):
+    for Info in range(len(Data)):
+        if Value == Data[Info]:
+            RemoveIndex.append(Info)
+    RemoveIndex.reverse()
+    for Info in RemoveIndex:
+        Keepdata.remove(Keepdata[Info])
+        Keepdata2.remove(Keepdata2[Info])
+        Keepdata3.remove(Keepdata3[Info])
+    typeShort = open("Shortcuts.txt", "w")
+    for Info in Keepdata:
+        typeShort.write(str(Info) + "\n")
+    typeShort.close()
+    typeUser = open("Usernames.txt", "w")
+    for Info in Keepdata2:
+        typeUser.write(str(Info) + "\n")
+    typeUser.close()
+    typePass = open("Passwords.txt", "w")
+    for Info in Keepdata3:
+        typePass.write(str(Info) + "\n")
+    typePass.close()
+    print("Removed " + str(len(RemoveIndex)) + " stored data lines")
 
 #Find function and correspondding code
 
@@ -62,40 +110,27 @@ def find():
         Shortcut = input("What is the shortcut?: ")
         if not(Shortcut == ""):
             print("")
-            Findshortcut(Shortcut)
+            Finder(Shortcut, Keepdata)
             break
         Usernames = input("What is the username?: ")
         if not(Usernames == ""):
             print("")
-            FindUsernames(Usernames)
+            Finder(Usernames, Keepdata2)
             break
         Password = input("What is the password?: ")
         if not(Password == ""):
             print("")
-            Findpassword(Password)
+            Finder(Password, Keepdata3)
             break
         print("")
 
-def Findshortcut(Shortcut):
+def Finder(Value, Data):
     print("Shortcut: Username:Password \n")
-    for Info in range(len(Keepdata)):
-        if Shortcut == Keepdata[Info]:
-            print(Keepdata[Info] + ": " + Keepdata2[Info] + ":" + Keepdata3[Info])
-           
-
-def FindUsernames(Usernames):
-    print("Shortcut: Username:Password \n")
-    for Info in range(len(Keepdata)):
-        if Usernames == Keepdata2[Info]:
+    for Info in range(len(Data)):
+        if Value == Data[Info]:
             print(Keepdata[Info] + ": " + Keepdata2[Info] + ":" + Keepdata3[Info])
 
-def Findpassword(Password):
-    print("Shortcut: Username:Password \n")
-    for Info in range(len(Keepdata)):
-        if Password == Keepdata3[Info]:
-            print(Keepdata[Info] + ": " + Keepdata2[Info] + ":" + Keepdata3[Info])
+#Read function and corresponding code
 
-#Open function and corresponding code
-
-def Open():
-    input("")
+def read():
+    print("")
