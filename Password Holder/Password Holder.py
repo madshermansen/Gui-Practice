a88b6c765ed3b2a49190c5ec9f5adf61b7aac73b
Mads Hermansen <33220981+KarlofKuwait@users.noreply.github.com>
2019-05-03 12:27:17 +0200

    Update
    SetHolder, Setnamelist, Shorter

--- Password Holder/Password Holder.py
+++ Password Holder/Password Holder.py
@@ -4,28 +4,13 @@ import PySimpleGUI as sg
 Shortcutread = "Shortcut"
 Usernameread = "Username"
 Passwordread = "Password"
-try:
-    names = [x.strip() for x in open("Shortcuts.txt").readlines()]
-    names2 = [x.strip() for x in open("Usernames.txt").readlines()]
-    names3 = [x.strip() for x in open("Passwords.txt").readlines()]
-    namelist = []
-    for Info in range(len(names)):
-        namelist.append(names[Info] + ": " + names2[Info] + ": " + names3[Info])
-except:
-    names = []
-
-
+namelist = PasswordHolderFunc.Setnamelist()
 
 while True:
-    layout = [
-        [sg.InputText(Shortcutread, size=(37, 1))],
-        [sg.InputText(Usernameread, size=(37, 1))],
-        [sg.InputText(Passwordread, size=(37, 1))],
-        [sg.Listbox(values=namelist, size=(35, 10))],
-        [sg.Submit("Add", size=(15, 1)), sg.Submit("Remove", size=(15, 1))],
-        [sg.Submit("Find", size=(15, 1)), sg.Submit("Open/Read", size=(15, 1))]
-    ]
-    Holder = sg.Window("Password Holder", icon='Logo/Logo.ico').Layout(layout)
+    try:
+        Holder = PasswordHolderFunc.SetHolder(Shortcutread, Usernameread, Passwordread, Positionx, namelist)
+    except:
+        Holder = PasswordHolderFunc.SetHolder(Shortcutread, Usernameread, Passwordread, None, namelist)
     event, values = Holder.Read()
     Shortcutread = ""
     Usernameread = ""
@@ -41,43 +26,15 @@ while True:
             Shortcutread, Usernameread, Passwordread = PasswordHolderFunc.read(values[3])
         except:
             Shortcutread, Usernameread, Passwordread = "", "", ""
-    try:
-        names = [x.strip() for x in open("Shortcuts.txt").readlines()]
-    except:
-        names = []
     if event == "Find":
         try:
-
             if values[0] == "" and values[1] == "" and values[2] == "":
-                try:
-                    names = [x.strip() for x in open("Shortcuts.txt").readlines()]
-                    names2 = [x.strip() for x in open("Usernames.txt").readlines()]
-                    names3 = [x.strip() for x in open("Passwords.txt").readlines()]
-                    namelist = []
-                    for Info in range(len(names)):
-                        namelist.append(names[Info] + ": " + names2[Info] + ": " + names3[Info])
-                except:
-                    namelist = []
+                namelist = PasswordHolderFunc.Setnamelist()
             else:
                 namelist = PasswordHolderFunc.find(values[0], values[1], values[2])
         except:
-            try:
-                names = [x.strip() for x in open("Shortcuts.txt").readlines()]
-                names2 = [x.strip() for x in open("Usernames.txt").readlines()]
-                names3 = [x.strip() for x in open("Passwords.txt").readlines()]
-                namelist = []
-                for Info in range(len(names)):
-                    namelist.append(names[Info] + ": " + names2[Info] + ": " + names3[Info])
-            except:
-                namelist = []
+            namelist = PasswordHolderFunc.Setnamelist()
     else:
-        try:
-            names = [x.strip() for x in open("Shortcuts.txt").readlines()]
-            names2 = [x.strip() for x in open("Usernames.txt").readlines()]
-            names3 = [x.strip() for x in open("Passwords.txt").readlines()]
-            namelist = []
-            for Info in range(len(names)):
-                namelist.append(names[Info] + ": " + names2[Info] + ": " + names3[Info])
-        except:
-            namelist = []
+        namelist = PasswordHolderFunc.Setnamelist()
+    Positionx = Holder.CurrentLocation()
     Holder.Close()
