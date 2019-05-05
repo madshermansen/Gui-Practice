fbbb90b6d3981c346860767ded9ac7d0c00ad6a4
Mads Hermansen <33220981+KarlofKuwait@users.noreply.github.com>
2019-05-05 16:19:22 +0200

    Update
    Menu
    Name

--- Password Holder/passwordholder.py
+++ Password Holder/passwordholder.py
@@ -25,20 +25,20 @@ while True:
     if event == "Exit" or event == None:
         break
     elif event == "Add":
-        passwordholderfunc.add(values[0], values[1], values[2])
+        passwordholderfunc.add(values[1], values[2], values[3])
     elif event == "Remove":
-        passwordholderfunc.remover(values[0], values[1], values[2], values[3])
+        passwordholderfunc.remover(values[1], values[2], values[3], values[4])
     elif event == "Open/Read":
         try:
-            Shortcutread, Usernameread, Passwordread = passwordholderfunc.read(values[3])
+            Shortcutread, Usernameread, Passwordread = passwordholderfunc.read(values[4])
         except:
             Shortcutread, Usernameread, Passwordread = "", "", ""
     if event == "Find":
         try:
-            if values[0] == "" and values[1] == "" and values[2] == "":
+            if values[1] == "" and values[2] == "" and values[3] == "":
                 namelist = passwordholderfunc.Setnamelist()
             else:
-                namelist = passwordholderfunc.find(values[0], values[1], values[2])
+                namelist = passwordholderfunc.find(values[1], values[2], values[3])
         except:
             namelist = passwordholderfunc.Setnamelist()
     else:
