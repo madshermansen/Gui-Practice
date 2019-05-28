6e13bea3147547a232ac5048fb88583d9df96ce2
Mads Hermansen <33220981+KarlofKuwait@users.noreply.github.com>
2019-05-28 23:27:44 +0200

    Update
    Bad GUI

--- Text To Ascii/Main.py
+++ Text To Ascii/Main.py
@@ -1,8 +1,19 @@
 from AsciiDic import *
+import PySimpleGUI as sg
 
-Word = input("Enter the word: ")
-for a in range(6):
-    printAsciivar = ""
-    for i in list(Word):
-        printAsciivar = printAsciivar + AsciiArt["Graffitti"][i][a]
-    print(printAsciivar)
+
+layout = [
+    [sg.InputText("", key=("Input"))],
+    [sg.Multiline("", size=(40, 20), key=("Output"))]
+]
+
+TranslatorWindow = sg.Window("Text To AsciiArt").Layout(layout)
+while True:
+    Event, Values = TranslatorWindow.ReadNonBlocking()
+    Word = Values["Input"]
+    for a in range(6):
+        printAsciivar = ""
+        for i in list(Word):
+            printAsciivar = printAsciivar + AsciiArt["Graffitti"][i][a]
+    TranslatorWindow.FindElement("Output").Update(printAsciivar)
+
